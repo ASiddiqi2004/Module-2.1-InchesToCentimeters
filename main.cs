b55f6f68225211150cf77a0216660b2a30d2a295
ASiddiqi2004 <amaansiddiqi@usf.edu>
2024-07-10 23:28:47 +0000

    Changed from reading manual integer to via console input

--- main.cs
+++ main.cs
@@ -3,8 +3,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    int inches = 3;
+    int inches;
     const double constcentimeters = 2.54;
+    Console.WriteLine ("Enter desired inches for conversion");
+    inches = Convert.ToInt32(Console.ReadLine());
     double centimeters = inches * constcentimeters;
     Console.WriteLine("{0} inches is {1} centimeters", inches, centimeters);
   }
